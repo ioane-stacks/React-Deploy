a27f34496ea6b61fce509a0a84f72e8d0fc7d778
Ioane-stacks <ioane.natroshvili08b@geolab.edu.ge>
2021-09-20 04:35:21 +0400

    commited

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
     <div className="App">
       <h1>Hello, I am Ionae</h1>
       <h2>This is my new React App on Github!</h2>
+      <h3>Header 3</h3>
       <img src={photo} alt="no photo" /> 
     </div>
   );
