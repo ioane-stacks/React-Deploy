73755e709187ac9c822ba344030c04b34e577b7f
Ioane-stacks <ioane.natroshvili08b@geolab.edu.ge>
2021-09-20 04:55:26 +0400

    added cat dialog

--- src/App.js
+++ src/App.js
@@ -5,8 +5,9 @@ function App() {
   return (
     <div className="App">
       <h1>Hello, I am Ionae</h1>
-      <h2>This is my new React App on Github!</h2>
-      <img src={photo} alt="no photo" /> 
+      <h2>Me: This is my new React App on Github!</h2>
+      <h3>Cat: No, you attempted to upload App fourth time unsuccessfully</h3>
+      <img src={photo} alt="no photo" />
     </div>
   );
 }
